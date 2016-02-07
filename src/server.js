71f0479577ede55372a881e2dadce5a81531fa15
Evan Wallace <evan.exe@gmail.com>
2016-02-07 14:10:54 -0800

    initial commit

---
+++ src/server.js
@@ -0,0 +1,138 @@
+var glslx = require('glslx');
+var server = require('vscode-languageserver');
+
+var buildResults = {};
+var workspaceRoot = null;
+var openDocuments = null;
+var connection = null;
+var timeout = null;
+
+function reportErrors(callback) {
+  try {
+    callback();
+  } catch (e) {
+    var message = e && e.stack || e;
+    connection.console.error('glslx: ' + message);
+    connection.window.showErrorMessage('glslx: ' + message);
+  }
+}
+
+function convertRange(range) {
+  return {
+    start: {
+      line: range.start.line,
+      character: range.start.column,
+    },
+    end: {
+      line: range.end.line,
+      character: range.end.column,
+    },
+  };
+}
+
+function sendDiagnostics(diagnostics) {
+  var map = {};
+
+  diagnostics.forEach(function(diagnostic) {
+    var key = diagnostic.range.source;
+    var group = map[key] || (map[key] = []);
+    group.push({
+      severity: diagnostic.kind === 'error' ? server.DiagnosticSeverity.Error : server.DiagnosticSeverity.Warning,
+      range: convertRange(diagnostic.range),
+      message: diagnostic.text,
+    });
+  });
+
+  openDocuments.all().forEach(function(doc) {
+    connection.sendDiagnostics({
+      uri: doc.uri,
+      diagnostics: map[doc.uri] || [],
+    });
+  });
+}
+
+function buildLater() {
+  clearTimeout(timeout);
+  timeout = setTimeout(function() {
+    reportErrors(function() {
+      var diagnostics = [];
+      var results = {};
+
+      openDocuments.all().forEach(function(doc) {
+        var result = glslx.compileIDE({
+          name: doc.uri,
+          contents: doc.getText(),
+        });
+        results[doc.uri] = result;
+        diagnostics.push.apply(diagnostics, result.diagnostics);
+      });
+
+      buildResults = results;
+      sendDiagnostics(diagnostics);
+    });
+  }, 100);
+}
+
+function computeTooltip(request) {
+  var result = buildResults[request.uri];
+
+  if (result) {
+    var response = result.tooltipQuery({
+      source: request.uri,
+      line: request.position.line,
+      column: request.position.character,
+
+      // Visual Studio Code already includes diagnostics and including
+      // them in the results causes each diagnostic to be shown twice
+      ignoreDiagnostics: true,
+    });
+
+    if (response.tooltip !== null) {
+      return {
+        contents: {
+          language: 'glslx',
+          value: response.tooltip,
+        },
+        range: convertRange(response.range),
+      }
+    }
+  }
+
+  return null;
+}
+
+function main() {
+  connection = server.createConnection(
+    new server.IPCMessageReader(process),
+    new server.IPCMessageWriter(process));
+
+  // Listen to open documents
+  openDocuments = new server.TextDocuments;
+  openDocuments.listen(connection);
+  openDocuments.onDidChangeContent(buildLater);
+
+  // Grab the workspace when the connection opens
+  connection.onInitialize(function(params) {
+    workspaceRoot = params.rootPath || null;
+    buildLater();
+    return {
+      capabilities: {
+        textDocumentSync: openDocuments.syncKind,
+        hoverProvider: true,
+      },
+    };
+  });
+
+  // Show tooltips on hover
+  connection.onHover(function(request) {
+    var tooltip = null;
+    reportErrors(function() {
+      tooltip = computeTooltip(request);
+    });
+    return tooltip;
+  });
+
+  connection.listen();
+}
+
+main();
